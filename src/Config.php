cce6ed4932befafd75e7433125bb9b84da291840
François Kooman <fkooman@tuxed.net>
2017-02-16 11:30:48 +0100

    update Config class

--- src/Config.php
+++ src/Config.php
@@ -18,35 +18,60 @@
 
 namespace SURFnet\VPN\Token;
 
+use SURFnet\VPN\Token\Exception\ConfigException;
+
 class Config
 {
     /** @var array */
     private $data;
 
+    /**
+     * @param array $data
+     */
     public function __construct(array $data)
     {
         $this->data = $data;
     }
 
+    /**
+     * @param string $key
+     */
     public function __isset($key)
     {
         return array_key_exists($key, $this->data);
     }
 
+    /**
+     * @param string $key
+     *
+     * @return object|string|array
+     */
     public function __get($key)
     {
         if (!array_key_exists($key, $this->data)) {
-            return null;
+            // consumers MUST check first if a field is available before
+            // requesting it
+            throw new ConfigException(sprintf('missing field "%s" in configuration', $key));
         }
 
         if (is_array($this->data[$key])) {
+            // if all we get is a "flat" array with sequential numeric keys
+            // return the array instead of an object
+            $k = array_keys($this->data[$key]);
+            if ($k === range(0, count($k) - 1)) {
+                return $this->data[$key];
+            }
+
             return new self($this->data[$key]);
         }
 
         return $this->data[$key];
     }
 
-    public function toArray()
+    /**
+     * @return array
+     */
+    public function asArray()
     {
         return $this->data;
     }
